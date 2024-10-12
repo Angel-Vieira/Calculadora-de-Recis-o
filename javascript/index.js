871cf9e5c52fcc1573d7c6b4c7f5820bd092981e
Angel Vieira <correiaa129@gmail.com>
2024-10-12 20:36:41 -0300

    Subindo versão 3.0

--- javascript/index.js
+++ javascript/index.js
@@ -21,24 +21,27 @@ document.querySelector(".button").addEventListener("click", function () {
  
     // Calculando o tempo de serviço em meses
     const diffTime = Math.abs(dataDemissao - dataAdmissao);
-    let diffMonths = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30)); // Aproximado para meses
+    let mesestotais = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30)); // Aproximado para meses
  
     // Limitando o número de meses a 12, para considerar apenas o último ano
-    diffMonths = Math.min(diffMonths, 12);
+    
+    mesesminimos = Math.min(mesestotais, 12);
  
     // Cálculo de férias e décimo terceiro proporcionais
-    const feriasProporcionais = ((salarioBruto / 12) * diffMonths) +((salarioBruto / 12) * diffMonths)/3 ;
-    const decimoTerceiroProporcional = (salarioBruto / 12) * diffMonths;
+    const feriasProporcionais = ((salarioBruto / 12) * mesesminimos) +((salarioBruto / 12) * mesesminimos)/3 ;
+    const decimoTerceiroProporcional = (salarioBruto / 12) * mesestotais;
+    console.log(mesesminimos)
  
     // Valor total da rescisão
     const valorRecisao = feriasProporcionais + decimoTerceiroProporcional;
+    
  
     // Mostrando o resultado na div "mostrarResultado"
     const resultado = document.getElementById("mostrarResultado");
     resultado.innerHTML = `
         <h2>Resultado da Rescisão</h2>
         <p>Salário Bruto: R$ ${salarioBruto.toFixed(2)}</p>
-        <p>Tempo de Serviço: ${diffMonths} meses</p>
+        <p>Tempo de Serviço: ${mesestotais} meses</p>
         <p>Férias Proporcionais: R$ ${feriasProporcionais.toFixed(2)}</p>
         <p>Décimo Terceiro Proporcional: R$ ${decimoTerceiroProporcional.toFixed(2)}</p>
         <p><strong>Valor Total da Rescisão: R$ ${valorRecisao.toFixed(2)}</strong></p>
